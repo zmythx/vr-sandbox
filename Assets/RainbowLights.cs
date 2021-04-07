f3175affbdb611729736b6ec9e258dfe6832ed9d
Zach Brewer <brewe057@umn.edu>
2021-04-07 01:01:18 -0500

    Added spaceship level assets

---
+++ Assets/RainbowLights.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowLights : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Renderer objRender;
+    public Material matToChange;
+    float curHue = 0;
+    public float ColorSpeed = 2;
+    void Start()
+    {
+        objRender = this.GetComponent<Renderer>();
+       /* foreach(Material mat in objRender.materials)
+        {
+            if(mat.name=="Material.002")
+            {
+                matToChange = mat;
+            }
+        }
+        if(matToChange == null)
+        {
+            matToChange = objRender.material;
+        }*/
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(curHue >= 1)
+        {
+            curHue = 0;
+        }
+        curHue += 0.001f * ColorSpeed;
+        matToChange.SetColor("_Color", Color.HSVToRGB(curHue, 1, 1));
+    }
+}
